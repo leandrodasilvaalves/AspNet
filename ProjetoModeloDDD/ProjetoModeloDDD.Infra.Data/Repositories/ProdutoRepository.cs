9a917b2b0c16ff15b73493c216e458c41c773eb2
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2017-12-26 20:22:25 -0200

    Projeto Modelo DDD - Video Eduardo Pires

---
+++ ProjetoModeloDDD/ProjetoModeloDDD.Infra.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Domain.Interfaces.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoModeloDDD.Infra.Data.Repositories
+{
+    public class ProdutoRepository : RepositoryBase<Produto>, IProdutoRepository
+    {
+        public IEnumerable<Produto> BuscarPorNome(string nome)
+        {
+            return db.Produtos.Where(p => p.Nome.Contains(nome));
+        }
+    }
+}
