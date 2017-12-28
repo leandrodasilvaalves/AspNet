c06178fa3924fa4fd558a15fbb0a7cc3e65c50a7
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2017-12-28 19:01:49 -0200

    Projeto MVC Areas

---
+++ MVCareas/MVCareas/Areas/Admin/Controllers/ProdutosController.cs
@@ -0,0 +1,128 @@
+﻿using MVCareas.Models;
+using System.Data.Entity;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MVCareas.Areas.Admin.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private Contexto db = new Contexto();
+
+        // GET: Admin/Produtos
+        public async Task<ActionResult> Index()
+        {
+            var produtos = db.Produtos.Include(p => p.Categoria);
+            return View(await produtos.ToListAsync());
+        }
+
+        // GET: Admin/Produtos/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = await db.Produtos.FindAsync(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // GET: Admin/Produtos/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoriaId = new SelectList(db.Categorias, "CategoriaId", "Descricao");
+            return View();
+        }
+
+        // POST: Admin/Produtos/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ProdutoId,Nome,Preco,CategoriaId")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Produtos.Add(produto);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoriaId = new SelectList(db.Categorias, "CategoriaId", "Descricao", produto.CategoriaId);
+            return View(produto);
+        }
+
+        // GET: Admin/Produtos/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = await db.Produtos.FindAsync(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoriaId = new SelectList(db.Categorias, "CategoriaId", "Descricao", produto.CategoriaId);
+            return View(produto);
+        }
+
+        // POST: Admin/Produtos/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ProdutoId,Nome,Preco,CategoriaId")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(produto).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoriaId = new SelectList(db.Categorias, "CategoriaId", "Descricao", produto.CategoriaId);
+            return View(produto);
+        }
+
+        // GET: Admin/Produtos/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = await db.Produtos.FindAsync(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // POST: Admin/Produtos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Produto produto = await db.Produtos.FindAsync(id);
+            db.Produtos.Remove(produto);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVCareas/MVCareas/Models/Contexto.cs
@@ -0,0 +1,40 @@
+﻿using MVCareas.Models.EntityConfig;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace MVCareas.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() :base("DefaultConnection")
+        {
+        }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+
+
+            modelBuilder.Configurations.Add(new ProdutoConfig());
+            modelBuilder.Configurations.Add(new CategoriaConfig());
+        }
+    }
+}
