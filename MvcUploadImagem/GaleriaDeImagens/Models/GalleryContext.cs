46d0b2a6916f073e309c1755875e52ace3ec8327
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2018-02-03 17:58:41 -0200

    Adicionado Projeto Galeria de Imagens

---
+++ MvcUploadImagem/GaleriaDeImagens/Models/GalleryContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace GaleriaDeImagens.Models
+{
+    public class GalleryContext : DbContext
+    {       
+        public GalleryContext():base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
---
+++ MvcUploadImagem/GaleriaDeImagens/Models/Photo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GaleriaDeImagens.Models
+{
+    public class Photo
+    {
+        [Key]
+        public int PhotoId { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required]
+        public String Description { get; set; }
+
+        [Display(Name = "Image Path")]
+        public String ImagePath { get; set; }
+
+        [Display(Name = "Thumb Path")]
+        public String ThumbPath { get; set; }
+
+
+        [Display(Name = "Criada em")]
+        public DateTime CreatedOn { get; set; }
+    }
+
+}
